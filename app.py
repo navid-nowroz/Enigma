dc27a27466b61d7cd2f34399ad3817f787144250
Twoki(Mac) <navidnowroz.s@gmail.com>
2026-04-11 11:15:21 +0600

    completed the machine route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from enigma.components import EnigmaCircuit, Rotor, Reflector, Plugboard
 from cli import Cr_rotor, Cr_plugboard, Cr_reflector
 
 # Importing from flask 
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, jsonify
 
 # Initiating flask app
 app = Flask(__name__)
@@ -11,9 +11,8 @@ app.secret_key = "Ayman is a little dickhead to take all the credits on the slid
 
 
 def SESSION_CHECKER():
-        return (("rotors" in session["enigma_config"]) and 
-        ("mode" in session["enigma_config"]) and 
-        ("reflector" in session["enigma_config"]))
+        cfg = session.get("enigma_config", {})
+        return all(k in cfg for k in ["rotors", "mode", "reflector"])
 
 
 # Defining the form parsing function
@@ -47,7 +46,7 @@ def encrypt():
         try: 
                 message = request.form.get("Message", "")
                 if not message:
-                        return(jsonify({"ok": False, "error": ""}))
+                        return(jsonify({"ok": False, "error": "No message provided"}))
 
                 enigma = build_circuit_from_session()
                 # Encrypt letter-by-letter, updating rotor windows as you go 
@@ -74,13 +73,13 @@ def setup():
                 if not (SESSION_CHECKER()):
                         return render_template("setup.html")
                 elif SESSION_CHECKER():
-                        return redirect(f"{{url_for('machine')}}")
+                        return redirect(url_for('machine'))
 
-        elif request.method() == "POST":
+        elif request.method == "POST":
                 try:
                         rotors, mode, reflector_choice, plugs = validate_and_parse_form(request.form)
                 except ValueError as e:
-                        return jsonify({"ok": False, "error": str(e)}, 400)
+                        return jsonify({"ok": False, "error": str(e)}), 400
 
                 # Save configuration in session (safe serializable data)
                 session["enigma_config"] = {
@@ -95,7 +94,7 @@ def setup():
 @app.route("/machine", methods = ["POST", "GET"])
 def machine():
         if request.method == "GET" and not (SESSION_CHECKER()):
-                return redirect("{{url_for('index')}}")
+                return redirect(url_for('index'))
         elif request.method == "GET" and (SESSION_CHECKER()):
                 ...
 
